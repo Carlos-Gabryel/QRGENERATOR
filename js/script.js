bdc644bbe85cfa5f787611311fcc809665b7353c
N3th3rL0rd <hackerhel@gmail.com>
2022-08-25 12:19:32 -0300

    QR size update

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ generateBtn.addEventListener("click", () => {
     if (!qrValue || preValue === qrValue) return;
     preValue = qrValue;
     generateBtn.innerText = "Generating QR Code...";
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=${qrValue}`;
     qrImg.addEventListener("load", () => {
         wrapper.classList.add("active");
         generateBtn.innerText = "Generate QR Code";
